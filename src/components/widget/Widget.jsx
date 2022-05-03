a2b8ed259063ab2ee29de08100a51b99a7ac7c51
Sherkhan Tulkibay <sherkhan-92@bk.ru>
2022-05-04 00:01:46 +0600

    add widget

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./widget.scss";
+
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+
+const Widget = () => {
+    return (
+        <div className="widget">
+            <div className="left">
+                <span className="title">USERS</span>
+                <span className="counter">21312</span>
+                <span className="link">See all user</span>
+            </div>
+            <div className="right">
+                <div className="percentage positive">
+                    <KeyboardArrowUpOutlinedIcon/>
+                    20 %
+                </div>
+                <PersonOutlinedIcon className="icon"/>
+            </div>
+        </div>
+    )
+}
+
+export default Widget;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.scss";
 
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 
 
 
@@ -12,7 +13,12 @@ const Home = () => {
             <Sidebar />
             <div className="homeContainer">
                 <Navbar/>
-                home container
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
             </div>
         </div>
     )
